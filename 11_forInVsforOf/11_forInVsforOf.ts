59ee872812c16ec624bddbb708007a5ac8011e04
Krishnendu Mandal <krishnendu.mandal89@gmail.com>
2019-11-10 18:33:05 +0530

    for in vs for of

---
+++ 11_forInVsforOf/11_forInVsforOf.ts
@@ -0,0 +1,8 @@
+let colors = ["red", "green", "blue"];
+
+for (let index in colors) console.log(colors[index]);
+
+for (let color of colors) console.log(color);
+
+let str = "hello";
+for (let ch of str) console.log(ch);
---
+++ scripts/11_forInVsforOf/11_forInVsforOf.js
@@ -0,0 +1,14 @@
+"use strict";
+var colors = ["red", "green", "blue"];
+for (var index in colors)
+    console.log(colors[index]);
+for (var _i = 0, colors_1 = colors; _i < colors_1.length; _i++) {
+    var color = colors_1[_i];
+    console.log(color);
+}
+var str = "hello";
+for (var _a = 0, str_1 = str; _a < str_1.length; _a++) {
+    var ch = str_1[_a];
+    console.log(ch);
+}
+//# sourceMappingURL=11_forInVsforOf.js.map
